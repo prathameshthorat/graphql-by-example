044f32b2e093eca8de2ae752b32480c56be16685
Prathamesh Thorat <prathameshthorat@Prathameshs-MacBook-Pro-2.local>
2025-09-30 23:51:50 +0530

    Code-First vs Schema-First

---
+++ hello-world/server/server-code-first.js
@@ -0,0 +1,21 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { makeSchema, queryType } from 'nexus';
+
+const Query = queryType({
+  definition(t) {
+    t.string('greeting', {
+      resolve: () => 'Hello world!',
+    });
+  },
+});
+
+const schema = makeSchema({ types: [Query] });
+
+const server = new ApolloServer({ schema });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 9000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
